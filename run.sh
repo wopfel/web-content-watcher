749b5ab4bfbc0ce3c78eda950ff581a0e712b54e
Bernd Arnold <wopfel@gmail.com>
2020-01-20 21:17:39 +0100

    Added comments

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This file is used by a Docker container
+# It calls wcw in an endless loop
+
 cd /data
 while true ; do echo "[$(date)] Running command now..." ; /wcw ; sleep $(( 3600 + $(shuf -i 700-1000 -n 1) )) ; done
 
